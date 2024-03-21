748511a1758e21135a780feccf1ee1786d507171
MrSwizzer <155651796+MrSwizzer@users.noreply.github.com>
2024-03-21 11:53:13 +0100

    Update div amount and made them align in a 16x16 Grid

--- srcipt/srcipt.js
+++ srcipt/srcipt.js
@@ -1,6 +1,7 @@
 const container = document.querySelector("#container");
 
-for (let index = 0; index < 32; index++) {
+for (let index = 0; index < (16*16); index++) {
     const div = document.createElement("div");
+    div.classList.add("grid_div")
     container.appendChild(div);
 }
