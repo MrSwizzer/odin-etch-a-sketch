32431c0f9032e072ff3b73a0b75b040a5af2f9aa
MrSwizzer <155651796+MrSwizzer@users.noreply.github.com>
2024-03-21 12:06:34 +0100

    Add mouseover Eventlistener to the divs that changes their color

--- srcipt/srcipt.js
+++ srcipt/srcipt.js
@@ -2,6 +2,14 @@ const container = document.querySelector("#container");
 
 for (let index = 0; index < (16*16); index++) {
     const div = document.createElement("div");
-    div.classList.add("grid_div")
+    div.classList.add("grid_div");
     container.appendChild(div);
 }
+
+const gridDivs = document.querySelectorAll(".grid_div");
+
+gridDivs.forEach((gridDiv) => {
+    gridDiv.addEventListener("mouseover", () =>{
+        gridDiv.style.backgroundColor = "orange"
+    })
+})
