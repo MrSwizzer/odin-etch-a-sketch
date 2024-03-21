e2c378755403f356719a4b3aec3d918e00fe7a00
MrSwizzer <155651796+MrSwizzer@users.noreply.github.com>
2024-03-21 15:26:20 +0100

    Add User Input to choose grid size nad changed color to randomized color

--- srcipt/srcipt.js
+++ srcipt/srcipt.js
@@ -1,15 +1,53 @@
 const container = document.querySelector("#container");
 
-for (let index = 0; index < (16*16); index++) {
-    const div = document.createElement("div");
-    div.classList.add("grid_div");
-    container.appendChild(div);
+function createDivs(squareSize, squaresPerSide, parentNode) {
+    for (let index = 0; index < squaresPerSide * squaresPerSide; index++) {
+        const div = document.createElement("div");
+        div.classList.add("grid_div");
+        div.style.width = `${squareSize}px`;
+        div.style.height = `${squareSize}px`;
+        parentNode.appendChild(div);
+    }
 }
 
-const gridDivs = document.querySelectorAll(".grid_div");
+function changeDivColor(randomColor) {
+    const gridDivs = document.querySelectorAll(".grid_div");
 
-gridDivs.forEach((gridDiv) => {
-    gridDiv.addEventListener("mouseover", () =>{
-        gridDiv.style.backgroundColor = "orange"
-    })
-})
+    gridDivs.forEach((gridDiv) => {
+        gridDiv.addEventListener("mouseover", () => {
+            color = getRandomColor();
+            gridDiv.style.backgroundColor = `${color}`;
+        });
+    });
+}
+
+function removePreviousGrid(parentNode) {
+    while (container.firstChild) {
+        parentNode.removeChild(container.firstChild);
+    }
+}
+
+function getUserInput() {
+    let userInput = parseInt(prompt("Enter the number of Squares per Side"));
+    if (userInput > 100) userInput = 100;
+    return userInput;
+}
+
+function getRandomColor() {
+    const letter = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
+    let color = "#";
+    for (let index = 0; index < 6; index++) {
+        color += letter[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+const button = document.querySelector("#userpopup");
+button.addEventListener("click", () => {
+    const squaresPerSide = getUserInput();
+    const squareSize = 960 / squaresPerSide;
+    removePreviousGrid(container);
+    createDivs(squareSize, squaresPerSide, container);
+    changeDivColor();
+    
+});
